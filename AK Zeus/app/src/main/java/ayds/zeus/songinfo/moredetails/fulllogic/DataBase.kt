df4e972c3012533f461639b39a28ce804e5780ce
GabrielTDP <franciscogpetz@gmail.com>
2021-05-08 22:46:36 -0300

    Ajustes en DataBase.kt y eliminación de DataBase.java
    El archivo DataBase.java fue eliminado y se realizaron ajustes en el archivo kotlin, algunas funciones ahora son estáticas usando el companion object

--- AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/fulllogic/DataBase.kt
+++ AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/fulllogic/DataBase.kt
@@ -10,82 +10,84 @@ import java.sql.*
 
 
 class DataBase(private val context: Context): SQLiteOpenHelper(context,"dictionary.db", null, 1) {
-    fun testDB(){
-        var connection:Connection? = null
-        try{
-            connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db")
-            val statement = connection.createStatement()
-            statement.queryTimeout = 30
-            val artistsResultSet = statement.executeQuery("select * from artists")
-            while(artistsResultSet.next()){
-                println("id = ${artistsResultSet.getInt("id")}")
-                println("artist = ${artistsResultSet.getString("artist")}")
-                println("info = ${artistsResultSet.getString("info")}")
-                println("source = ${artistsResultSet.getString("source")}")
-            }
-        }catch (e: SQLException){
-            println(e.message)
-        }finally{
-            try{
-                connection?.close()
-            }catch (e: SQLException){
+    companion object {
+        @JvmStatic fun testDB() {
+            var connection: Connection? = null
+            try {
+                connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db")
+                val statement = connection.createStatement()
+                statement.queryTimeout = 30
+                val artistsResultSet = statement.executeQuery("select * from artists")
+                while (artistsResultSet.next()) {
+                    println("id = ${artistsResultSet.getInt("id")}")
+                    println("artist = ${artistsResultSet.getString("artist")}")
+                    println("info = ${artistsResultSet.getString("info")}")
+                    println("source = ${artistsResultSet.getString("source")}")
+                }
+            } catch (e: SQLException) {
                 println(e.message)
+            } finally {
+                try {
+                    connection?.close()
+                } catch (e: SQLException) {
+                    println(e.message)
+                }
             }
         }
-    }
 
-    fun saveArtist(dbHelper: DataBase, artist: String, info: String ){
-        val dataBase = dbHelper.writableDatabase
-        val contentValues = createArtistContentValues(artist, info)
-        dataBase.insert("artists", null, contentValues)
-    }
+        @JvmStatic fun saveArtist(dbHelper: DataBase, artist: String, info: String) {
+            val dataBase = dbHelper.writableDatabase
+            val contentValues = createArtistContentValues(artist, info)
+            dataBase.insert("artists", null, contentValues)
+        }
 
-    private fun createArtistContentValues(artist: String, info: String) = ContentValues().apply {
-        this.put("artist", artist)
-        this.put("info", info)
-        this.put("source", 1)
-    }
+        @JvmStatic fun getInfo(dbHelper: DataBase, artist: String): String? {
+            val cursor = getNewArtistCursor(dbHelper, artist)
+            val items = getCursorItems(cursor)
+            cursor.close()
+            return if (items.isEmpty())
+                null
+            else
+                items[0]
+        }
 
-    fun getInfo(dbHelper: DataBase, artist: String) : String?{
-        val cursor = getNewArtistCursor(dbHelper, artist)
-        val items = getCursorItems(cursor)
-        cursor.close()
-        return if (items.isEmpty())
-            null
-        else
-            items[0]
-    }
+        private fun createArtistContentValues(artist: String, info: String) = ContentValues().apply {
+            this.put("artist", artist)
+            this.put("info", info)
+            this.put("source", 1)
+        }
 
-    private fun getNewArtistCursor(dbHelper: DataBase, artist: String): Cursor {
-        val dataBase = dbHelper.readableDatabase
-        val projection = arrayOf("id", "artist", "info")
-        val selection = "artist = ?"
-        val selectionArgs = arrayOf(artist)
-        val sortOrder = "artist DESC"
-        return dataBase.query(
-                "artists",
-                projection,
-                selection,
-                selectionArgs,
-                null,
-                null,
-                sortOrder
-        )
-    }
+        private fun getNewArtistCursor(dbHelper: DataBase, artist: String): Cursor {
+            val dataBase = dbHelper.readableDatabase
+            val projection = arrayOf("id", "artist", "info")
+            val selection = "artist = ?"
+            val selectionArgs = arrayOf(artist)
+            val sortOrder = "artist DESC"
+            return dataBase.query(
+                    "artists",
+                    projection,
+                    selection,
+                    selectionArgs,
+                    null,
+                    null,
+                    sortOrder
+            )
+        }
 
-    private fun getCursorItems(cursor: Cursor) = ArrayList<String>().apply {
-        while (cursor.moveToNext()){
-            val columnIndex = cursor.getColumnIndexOrThrow("info")
-            val info = cursor.getString(columnIndex)
-            this.add(info)
+        private fun getCursorItems(cursor: Cursor) = ArrayList<String>().apply {
+            while (cursor.moveToNext()){
+                val columnIndex = cursor.getColumnIndexOrThrow("info")
+                val info = cursor.getString(columnIndex)
+                this.add(info)
+            }
         }
     }
 
     override fun onCreate(db: SQLiteDatabase){
-
+        db.execSQL(
+                "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)")
+        Log.i("DB", "DB created")
     }
 
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int){
-
-    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int){}
 }
