eb0669b63d84f2fa029c66c38305a1eefd03ba4b
Fabricio <39932090+Fabri31295@users.noreply.github.com>
2021-05-22 17:18:41 -0300

    MoreDetailsController

--- AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/fulllogic/controller/MoreDetailsController.kt
+++ AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/fulllogic/controller/MoreDetailsController.kt
@@ -1,10 +1,27 @@
 package ayds.zeus.songinfo.moredetails.fulllogic.controller
 
-interface FullLogicController{
-    fun setFullLogic()
+import ayds.zeus.songinfo.moredetails.fulllogic.model.MoreDetailsModel
+import ayds.zeus.songinfo.moredetails.fulllogic.view.MoreDetailsView
+
+interface MoreDetailsController{
+    fun setMoreDetailsView(moreDetailsView: MoreDetailsView)
 }
 
-internal class FullLogicControllerIMP{
+internal class MoreDetailsControllerImpl(private val moreDetailsModel: MoreDetailsModel) : MoreDetailsController {
+
+    private lateinit var moreDetailsView: MoreDetailsView
+
+    override fun setMoreDetailsView(moreDetailsView: MoreDetailsView) {
+        this.moreDetailsView = moreDetailsView
+    }
+
+    private fun viewFullArticle(){
+
+    }
+
+    private fun openWikipediaURL(){
+        
+    }
 
 }
 
