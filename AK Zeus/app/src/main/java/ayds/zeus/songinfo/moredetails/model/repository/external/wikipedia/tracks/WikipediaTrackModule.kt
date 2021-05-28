c5c78e930a8b34c0406bda1f0176606db5858e72
GabrielTDP <franciscogpetz@gmail.com>
2021-05-28 13:46:44 -0300

    correcciones al wikipediaTrackModule

--- AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/model/repository/external/wikipedia/tracks/WikipediaTrackModule.kt
+++ AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/model/repository/external/wikipedia/tracks/WikipediaTrackModule.kt
@@ -1,8 +1,19 @@
 package ayds.zeus.songinfo.moredetails.model.repository.external.wikipedia.tracks
 
 import ayds.zeus.songinfo.moredetails.model.repository.external.wikipedia.WikipediaService
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object WikipediaTrackModule {
-
-    val wikipediaService: WikipediaService
+    private const val URL_WIKIPEDIA = "https://en.wikipedia.org/w/"
+    private val wikipediaAPIretrofit = Retrofit.Builder()
+            .baseUrl(URL_WIKIPEDIA)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    private val wikipediaAPI = wikipediaAPIretrofit.create(WikipediaAPI::class.java)
+    private val wikipediaToArticleResolver: WikipediaToArticleResolver = JsonToArticleResolver()
+    val wikipediaService: WikipediaService = WikipediaServiceImpl(
+            wikipediaToArticleResolver,
+            wikipediaAPI
+    )
 }
