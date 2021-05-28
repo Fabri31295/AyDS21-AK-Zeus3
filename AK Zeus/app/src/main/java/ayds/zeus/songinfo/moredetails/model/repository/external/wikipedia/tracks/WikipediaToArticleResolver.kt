0e47ba6c795c34cc2d6cd0301c2110e72822be1f
Agus M <agus91997@gmail.com>
2021-05-28 13:09:29 -0300

    ArticleResolver implementación inicial

--- AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/model/repository/external/wikipedia/tracks/WikipediaToArticleResolver.kt
+++ AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/model/repository/external/wikipedia/tracks/WikipediaToArticleResolver.kt
@@ -2,7 +2,10 @@ package ayds.zeus.songinfo.moredetails.model.repository.external.wikipedia.track
 
 import ayds.zeus.songinfo.moredetails.model.entities.WikipediaArticle
 import com.google.gson.Gson
+import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import retrofit2.Response
+import retrofit2.http.Body
 
 interface WikipediaToArticleResolver{
     fun getArticleFromExternalData(serviceData: String?): WikipediaArticle?
@@ -10,18 +13,25 @@ interface WikipediaToArticleResolver{
 
 private const val JSON_SNIPPET = "snippet"
 private const val JSON_PAGE_ID = "pageid"
-private const val IMAGE_WIKIPEDIA =
-        "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png"
 private const val URL_WIKIPEDIA = "https://en.wikipedia.org/w/"
 private const val WIKIPEDIA_SHORT_URL = "https://en.wikipedia.org/?curid="
 private const val JSON_QUERY = "query"
 private const val JSON_SEARCH = "search"
+private const val JSON_TITLE = "title"
 
 internal class JsonToArticleResolver : WikipediaToArticleResolver{
+
+    private lateinit var artistName: String
+    private lateinit var wikipediaAPI: WikipediaAPI
+
     override fun getArticleFromExternalData(serviceData: String?): WikipediaArticle? =
         try{
-            serviceData?.getResponseJson().let {
-                TODO("FALTA CREAR EL ARTICLE")
+            serviceData?.getResponseJson()?.let { item ->
+                WikipediaArticle(
+                    item.getName(),
+                    item.getInfo(),
+                    item.getUrl()
+                )
             }
         }catch(e: Exception){
             null
@@ -32,4 +42,38 @@ internal class JsonToArticleResolver : WikipediaToArticleResolver{
         return gson.fromJson(this, JsonObject::class.java)
     }
 
+    private fun JsonObject.getName(): String {
+        return this[JSON_TITLE].asString
+    }
+
+    private fun JsonObject.getInfo(): String {
+        TODO("Refactorizar")
+        val snippet = getDataFromResponse(JSON_SNIPPET)
+        return snippet.asString.replace("\\n", "\n")
+    }
+
+    private fun JsonObject.getUrl(): String {
+        return WIKIPEDIA_SHORT_URL + this[JSON_PAGE_ID].asString
+    }
+
+    private fun getDataFromResponse(name: String): JsonElement {
+        val jobj = getResponseJson()
+        return getDataFromJson(jobj, name)
+    }
+
+    private fun getResponseJson(): JsonObject {
+        val callResponse = getCallResponse()
+        val gson = Gson()
+        return gson.fromJson(callResponse.body(), JsonObject::class.java)
+    }
+
+    private fun getDataFromJson(jobj: JsonObject, name: String): JsonElement {
+        val query = jobj[JSON_QUERY].asJsonObject
+        return query[JSON_SEARCH].asJsonArray[0].asJsonObject[name]
+    }
+
+    private fun getCallResponse(): Response<String> {
+        return wikipediaAPI.getArtistInfo(artistName).execute() //repetido, debe estar mal
+    }
 }
+
