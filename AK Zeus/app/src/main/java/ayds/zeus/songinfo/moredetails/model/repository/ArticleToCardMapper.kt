dad4333db54fc20d7d7b30017fabc509a6a27a05
Agus M <agus91997@gmail.com>
2021-06-22 17:07:02 -0300

    submodulo - reemplazo interface Article con clase WikipediaArticle

--- AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/model/repository/ArticleToCardMapper.kt
+++ AK Zeus/app/src/main/java/ayds/zeus/songinfo/moredetails/model/repository/ArticleToCardMapper.kt
@@ -2,14 +2,14 @@ package ayds.zeus.songinfo.moredetails.model.repository
 
 import ayds.zeus.songinfo.moredetails.model.repository.entities.Card
 import ayds.zeus.songinfo.moredetails.model.repository.entities.Source
-import ayds.zeus3.wikipedia.Article
+import ayds.zeus3.wikipedia.WikipediaArticle
 
 interface ArticleToCardMapper {
-    fun map(article: Article, source: Source): Card
+    fun map(article: WikipediaArticle, source: Source): Card
 }
 
 internal class ArticleToCardMapperImpl : ArticleToCardMapper {
-    override fun map(article: Article, source: Source) = Card(
+    override fun map(article: WikipediaArticle, source: Source) = Card(
         article.info,
         article.url,
         article.logoUrl,
